89f9212cfe39110bf0ca61f79894565d39594781
EricChv <101233505+EricChv@users.noreply.github.com>
2024-10-15 15:52:35 -0400

    feat: add HeaderBox component to display username and greeting message

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+    const loggedIn = { firstName: "Eric"};
+
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+            <HeaderBox 
+                type="greeting"
+                title="Welcome back,"
+                user={loggedIn?.firstName + '!' || "Guest"}
+                subtext="Conveniently manage all your financial needs in one place."
+            />
+
+            <TotalBalanceBox
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={4500} // temp balance placeholder
+            />
+        </header>
+      </div>
+    </section>
   )
 }
 
