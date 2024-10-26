98bbc906390e47d3ae0fecb6832a340682488b12
EricChv <101233505+EricChv@users.noreply.github.com>
2024-10-26 13:50:38 -0400

    Implement sidebar with icons for Home, Bank, Transaction History, and Transfer Funds

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,63 @@
+'use client'
+
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
-const Sidebar = ({user}: SidebarProps) => {
-  return (
-    <div>
-      Sidebar
-    </div>
-  )
+const Sidebar = ({ user }: SidebarProps) => {
+    const pathname = usePathname();
+
+    return (
+        <section className="sidebar">
+            <nav className="flex flex-col gap-4">
+
+                // Logo
+                <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+                    <Image 
+                        src="/icons/logo.svg"
+                        width={34}
+                        height={34}
+                        alt="MyFinance logo"
+                        className="size-[24px] max-xl:size-14"
+                    />
+                    <h1 className="sidebar-logo">MyFinance</h1>
+                </Link>
+
+                {sidebarLinks.map((items) => {
+                    const isActive = pathname === items.route || pathname.startsWith(`${items.route}/`)
+                    
+                    return (
+                        <Link href={items.route} key={items.label}
+                            className={cn('sidebar-link', {'bg-bank-gradient': isActive})}
+                            >
+                                <div className="relative size-6">
+                                    <Image 
+                                        src={items.imgURL}
+                                        alt={items.label}
+                                        fill
+                                        className={cn({
+                                            'brightness-[3] invert-0': isActive
+                                        })}
+                                    />
+                                </div>
+
+                                {/* Text label for the sidebar items */}
+                                <p className={cn("sidebar-label", {"!text-white" :isActive})}> {/* Apply white text color if item is active  */}
+                                    {items.label} {/* Display the item's label text */}
+
+                                </p>
+                        </Link>
+                    )
+                })}
+
+                USER
+            </nav>
+            FOOTER
+        </section>
+    )
 }
 
 export default Sidebar
--- constants/index.ts
+++ constants/index.ts
@@ -32,7 +32,7 @@ export const TEST_USER_ID = "6627ed3d00267aa6fa3e";
 export const TEST_ACCESS_TOKEN =
   "access-sandbox-229476cf-25bc-46d2-9ed5-fba9df7a5d63";
 
-export const ITEMS = [
+export const items = [
   {
     id: "6624c02e00367128945e", // appwrite item Id
     accessToken: "access-sandbox-83fd9200-0165-4ef8-afde-65744b9d1548",
