ac01d7900581b4132ff1072a835db148f51e4bea
EricChv <101233505+EricChv@users.noreply.github.com>
2024-12-31 10:34:17 -0500

    feat: add TransactionsTable component for displaying transaction data
    - Introduced a  component for rendering transaction details in a tabular format.
    - Utilized , , , , , , and  from the UI library.
    - Enhanced transaction display with formatted amounts, statuses, and dates using utility functions (, , , ).
    - Added conditional styling for debit and credit transactions, with distinct colors for rows and amounts.
    - Included category column for larger screens while maintaining responsive design for smaller screens.

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -7,7 +7,28 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { formatAmount, getTransactionStatus } from "@/lib/utils"
+import { transactionCategoryStyles } from "@/constants"
+import { cn, formatAmount, formatDateTime, getTransactionStatus, removeSpecialCharacters } from "@/lib/utils"
+
+const CategoryBadge = ({ category }: CategoryBadgeProps ) => {
+  const {
+    borderColor,
+    backgroundColor,
+    textColor,
+    chipBackgroundColor,
+  } = transactionCategoryStyles[category as keyof typeof transactionCategoryStyles] ||
+  transactionCategoryStyles.default
+
+
+  return (
+    <div className={cn('category-badge', borderColor, chipBackgroundColor)}>
+      <div className={cn('size-2 rounded-full', backgroundColor)} />
+      <p className={cn('text-[12px] font-medium', textColor)}>
+        {category}
+      </p>
+    </div>
+  )
+}
 
 const TransactionsTable = ({ transactions }: TransactionTableProps) => {
   return (
@@ -18,7 +39,6 @@ const TransactionsTable = ({ transactions }: TransactionTableProps) => {
           <TableHead className="px-2">Amount</TableHead>
           <TableHead className="px-2">Status</TableHead>
           <TableHead className="px-2">Date</TableHead>
-          <TableHead className="px-2 max-md:hidden">Channel</TableHead>
           <TableHead className="px-2 max-md:hidden">Category</TableHead>
         </TableRow>
       </TableHeader>
@@ -31,14 +51,33 @@ const TransactionsTable = ({ transactions }: TransactionTableProps) => {
           const isCredit = t.type === "credit";
 
           return (
-            <TableRow key={t.id}>
-              <TableCell>
-                <div>
-                  <h1>
-                    {t.name}
+            <TableRow key={t.id} className={`${isDebit || amount [0] === '-' 
+              ? 'bg-[#FFFBFA]' : 'bg-[#F6FEF9]'} !over:bg-none !border-b-DEFAULT`}>
+              <TableCell className="max-w-[250px] pl-2 pr-10">
+                <div className="flex item-center gap-3">
+                  <h1 className="text-14 truncate font-semibold text-[#344054]">
+                    {removeSpecialCharacters(t.name)}
                   </h1>
                 </div>
               </TableCell>
+
+              <TableCell className={`pl-2 pr-10 font-semibold 
+                ${isDebit || amount [0] === '-' ? 'text-[#f04438]' : 'text-[#039855]'}`}>
+                {isDebit ? `-${amount}` : isCredit ? amount : amount}
+              </TableCell>
+
+              <TableCell className="pl-2 pr-10">
+                <CategoryBadge category={status} />
+              </TableCell>
+
+              <TableCell className="min-w-32 pl-2 pr-10">
+                {formatDateTime(new Date(t.date)).dateTime}
+              </TableCell>
+
+              <TableCell className="pl-2 pr-10 max-md:hidden">
+                <CategoryBadge category={t.category} />
+              </TableCell>
+
             </TableRow>
           )
         })}
